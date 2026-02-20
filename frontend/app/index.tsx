b43c63c6e5fa7d8e88e70013a54e41846c3ef665
Luis Reyes <luis.reyes02@student.csulb.edu>
2026-02-20 04:15:15 -0800

    feat: render map on home screen

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,9 +1,29 @@
-import { Text, View } from "react-native";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import MapView from "react-native-maps";
 
-export default function Index() {
+// Initial region: CSULB
+const INITIAL_REGION = {
+  latitude: 33.7838,
+  longitude: -118.1141,
+  latitudeDelta: 0.05,
+  longitudeDelta: 0.05,
+};
+
+export default function App() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Hello world</Text>
+    <View style={styles.container}>
+      <MapView initialRegion={INITIAL_REGION} style={styles.map} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
