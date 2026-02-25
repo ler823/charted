06d7ba566b6141ff266787e12fb880654d45a971
Luis Reyes <luis.reyes02@student.csulb.edu>
2026-02-25 13:43:30 -0800

    icon showing on long press

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import MapView from "react-native-maps";
+
+import { Ionicons } from "@expo/vector-icons";
 
 // Initial region: CSULB
 const INITIAL_REGION = {
@@ -10,10 +12,20 @@ const INITIAL_REGION = {
   longitudeDelta: 0.05,
 };
 
-export default function App() {
+export default function Home() {
+  const [isPicking, setIsPicking] = useState(false);
   return (
     <View style={styles.container}>
-      <MapView initialRegion={INITIAL_REGION} style={styles.map} />
+      {isPicking && (
+        <View style={styles.crosshairContainer} pointerEvents="none">
+          <Ionicons name="location-sharp" size={40} color="red" />
+        </View>
+      )}
+      <MapView
+        initialRegion={INITIAL_REGION}
+        style={styles.map}
+        onLongPress={() => setIsPicking(true)}
+      />
     </View>
   );
 }
@@ -25,5 +37,12 @@ const styles = StyleSheet.create({
   map: {
     width: "100%",
     height: "100%",
+  },
+  crosshairContainer: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: [{ translateX: -20 }, { translateY: -40 }], // center the icon
+    zIndex: 10,
   },
 });
