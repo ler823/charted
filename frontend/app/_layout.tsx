b47cac4b561ce690d1f743b99bb82637602af84a
Kathryn Woest <kathryn.woest@gmail.com>
2026-03-02 08:05:22 -0800

    Add fonts to root _layout to force them to load before rendering app

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,6 +1,22 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import {
+  Raleway_200ExtraLight,
+  Raleway_400Regular,
+  Raleway_700Bold,
+} from "@expo-google-fonts/raleway";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    Raleway_200ExtraLight,
+    Raleway_400Regular,
+    Raleway_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null; // need to change this to be the splash page or a loading screen so that the app functions while fonts load in
+  }
+  
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: "Home" }} />
