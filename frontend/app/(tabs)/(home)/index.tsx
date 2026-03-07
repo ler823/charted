bfa789db2c7234f61aa5b09cac4f1c0126d93bb2
Luis Reyes <luis.reyes02@student.csulb.edu>
2026-03-07 10:14:59 -0800

    made header component, types folder/file

--- frontend/app/(tabs)/(home)/index.tsx
+++ frontend/app/(tabs)/(home)/index.tsx
@@ -1,17 +1,17 @@
 import { supabase } from "@/lib/supabase";
 import { Ionicons } from "@expo/vector-icons";
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import React, { useEffect, useState } from "react";
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import PinMarker from "../../../components/pin-marker";
-import { Fonts } from "../../../constants/fonts";
 import { Colors } from "../../../constants/theme";
 import PinListView from "./pin_list_view";
 
 import DroppingPinOverlay from "@/components/dropping-pin-overlay";
+import Header from "@/components/header";
 import { useDroppingPin } from "@/context/DroppingPinContext";
+import { Pin, ViewMode, ViewOption } from "@/types/types";
 
 // CSULB as initial region (for now)
 // Later it should be user's location if location services enabled
@@ -22,22 +22,11 @@ const INITIAL_REGION = {
   longitudeDelta: 0.05,
 };
 
-type ViewMode = "map" | "list" | "grid";
-
-type Pin = {
-  id: string;
-  name: string;
-  address: string;
-  latitude: number;
-  longitude: number;
-};
-
-const VIEW_OPTIONS: { mode: ViewMode; icon: keyof typeof Ionicons.glyphMap }[] =
-  [
-    { mode: "map", icon: "map" },
-    { mode: "list", icon: "list" },
-    { mode: "grid", icon: "grid" },
-  ];
+const VIEW_OPTIONS: ViewOption[] = [
+  { mode: "map", icon: "map" },
+  { mode: "list", icon: "list" },
+  { mode: "grid", icon: "grid" },
+];
 
 export default function Home() {
   const { isDroppingPin, setIsDroppingPin } = useDroppingPin();
@@ -62,7 +51,7 @@ export default function Home() {
           address: row.address,
           latitude: row.latitude ?? 0,
           longitude: row.longitude ?? 0,
-        }))
+        })),
       );
     }
     fetchLocations();
@@ -71,65 +60,19 @@ export default function Home() {
   return (
     <View style={styles.container}>
       {!isDroppingPin && (
-        <Pressable style={styles.plusButton} onPress={() => setIsDroppingPin(true)}>
+        <Pressable
+          style={styles.plusButton}
+          onPress={() => setIsDroppingPin(true)}
+        >
           <MaterialCommunityIcons name="plus" size={45} color="#fefbea" />
         </Pressable>
       )}
       {!isDroppingPin && (
-        <View style={styles.header}>
-
-          {/* Search Bar and Settings*/}
-          <View style={styles.row}>
-            <Pressable style={styles.searchbar}>
-              <Text style={{fontFamily: Fonts.bold, color: "#fefbea", fontSize: 16}}>
-                Find a place
-              </Text>
-              <FontAwesome name="search" size={20} color="#fefbea" />
-            </Pressable>
-            <View>
-              <Pressable style={styles.settings}>
-                <Ionicons name="settings" size={32} color="#243e36" />
-              </Pressable>
-            </View>
-          </View>
-
-          {/* Pill and Filter*/}
-          <View style={styles.row}>
-            <View style={styles.pill}>
-              {VIEW_OPTIONS.map(({ mode, icon }) => (
-                <Pressable
-                  key={mode}
-                  onPress={() => setViewMode(mode)}
-                  style={[
-                    styles.pillOption,
-                    viewMode === mode && (
-                      mode === "map" 
-                        ? styles.pillOptionActiveMap
-                        : viewMode === "list"
-                        ? styles.pillOptionActiveList
-                        : styles.pillOptionActiveGrid
-                    )
-                  ]}
-                >
-                  <Ionicons
-                    name={icon}
-                    size={20}
-                    color={viewMode === mode ? "#d9d9d9" : "#d9d9d9"}
-                  />
-                </Pressable>
-              ))}
-            </View>
-
-            <View>
-              <Pressable style={styles.filter}>
-                <Text style={{fontFamily: Fonts.bold, color: "#d9d9d9", fontSize: 16,}}>
-                  Filter
-                </Text>
-                <Ionicons name="chevron-down" size={20} color="#d9d9d9" />
-              </Pressable>
-            </View>
-          </View>
-        </View>
+        <Header
+          viewMode={viewMode}
+          setViewMode={setViewMode}
+          viewOptions={VIEW_OPTIONS}
+        />
       )}
 
       {viewMode === "map" && (
@@ -153,16 +96,21 @@ export default function Home() {
           {/* 
             controls pin marker, takes from pin-marker component
           */}
-          {pins.filter((pin) => pin.latitude !== 0 && pin.longitude !== 0).map((pin) => (
-            <Marker
-              key={pin.id}
-              coordinate={{ latitude: pin.latitude, longitude: pin.longitude }}
-              onPress={() => setSelectedPin(pin)}
-              tracksViewChanges={false}
-            >
-              <PinMarker />
-            </Marker>
-          ))}
+          {pins
+            .filter((pin) => pin.latitude !== 0 && pin.longitude !== 0)
+            .map((pin) => (
+              <Marker
+                key={pin.id}
+                coordinate={{
+                  latitude: pin.latitude,
+                  longitude: pin.longitude,
+                }}
+                onPress={() => setSelectedPin(pin)}
+                tracksViewChanges={false}
+              >
+                <PinMarker />
+              </Marker>
+            ))}
         </MapView>
       )}
       {viewMode === "list" && (
@@ -170,17 +118,13 @@ export default function Home() {
           <PinListView />
         </View>
       )}
-      {viewMode === "grid" && (
-        <View style={styles.placeholder}>
-        </View>
-      )}
+      {viewMode === "grid" && <View style={styles.placeholder}></View>}
       {/* 
         PIN OVERLAY
       */}
       {selectedPin && (
         <Pressable style={styles.backdrop} onPress={() => setSelectedPin(null)}>
           <Pressable style={styles.overlayCard} onPress={() => {}}>
-
             <View style={styles.picturePlaceholder}>
               <Ionicons name="image-outline" size={48} color="#bbb" />
             </View>
@@ -192,7 +136,6 @@ export default function Home() {
               </View>
               <Ionicons name="expand-outline" size={20} color="#555" />
             </View>
-
           </Pressable>
         </Pressable>
       )}
@@ -208,97 +151,11 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     flex: 1,
   },
-  header: {
-    position: "absolute",
-    top: 45,
-    paddingHorizontal: 15,
-    zIndex: 20,
-  },
-  row: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginTop: 15,
-  },
+
   cardsContainer: {
     flex: 1,
   },
-  searchbar: {
-    backgroundColor: "#7ca982",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    paddingHorizontal: 15,
-    width: 300,
-    height: 40,
-    borderRadius: 999,
-    zIndex: 20,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 4 },
-    shadowOpacity: 0.3,
-    shadowRadius: 2,
-    elevation: 4,
-  },
-  settings: {
-    backgroundColor: "#7ca982",
-    borderRadius: 999,
-    alignItems: "center",
-    justifyContent: "center",
-    height: 40,
-    width: 40,
-    marginLeft: 8,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 4 },
-    shadowOpacity: 0.3,
-    shadowRadius: 2,
-    elevation: 4,
-  },
-  pill: {
-    backgroundColor: "#243e36",
-    flexDirection: "row",
-    borderRadius: 999,
-    gap: 4,
-    zIndex: 20,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 4 },
-    shadowOpacity: 0.3,
-    shadowRadius: 2,
-    elevation: 4,
-  },
-  pillOption: {
-    paddingHorizontal: 16,
-    paddingVertical: 10,
-    borderRadius: 999,
-  },
-  pillOptionActiveMap: {
-    backgroundColor: "#7ca982",
-    borderTopRightRadius: 0,
-    borderBottomRightRadius: 0,
-  },
-  pillOptionActiveList: {
-    backgroundColor: "#7ca982",
-    borderRadius: 0,
-  },
-  pillOptionActiveGrid: {
-    backgroundColor: "#7ca982",
-    borderTopLeftRadius: 0,
-    borderBottomLeftRadius: 0,
-  },
-  filter: {
-    backgroundColor: "#243e36",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 999,
-    gap: 6,
-    width: 105,
-    height: 40,
-    marginLeft: 8,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 4 },
-    shadowOpacity: 0.3,
-    shadowRadius: 2,
-    elevation: 4,
-  },
+
   placeholder: {
     flex: 1,
     alignItems: "center",
@@ -348,16 +205,19 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     backgroundColor: "#243e36",
     borderRadius: 100,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOffset: { width: 0, height: 4 },
     shadowOpacity: 0.3,
     shadowRadius: 2,
-    elevation: 4
+    elevation: 4,
   },
   backdrop: {
     position: "absolute",
-    top: 0, left: 0, right: 0, bottom: 0,
-    backgroundColor: "transparent",   
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: "transparent",
     alignItems: "center",
     justifyContent: "center",
     zIndex: 30,
