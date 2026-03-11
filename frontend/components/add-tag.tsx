fd3531a0966b439fe832cfb5233bfc9766ae3739
kn2117 <89276194+kn2117@users.noreply.github.com>
2026-03-11 11:58:43 -0700

    Reduced tab size from 4 spaces to 2 in add-tag.tsx

--- frontend/components/add-tag.tsx
+++ frontend/components/add-tag.tsx
@@ -4,105 +4,105 @@ import { Keyboard, Modal, Pressable, StyleSheet, Text, TextInput, View } from 'r
 
 
 type Props = PropsWithChildren<{
-    isVisible: boolean;
-    onClose: () => void;
-    onSave: () => void;
-    newTag: string;
-    setNewTag: (value: string) => void;
+  isVisible: boolean;
+  onClose: () => void;
+  onSave: () => void;
+  newTag: string;
+  setNewTag: (value: string) => void;
 }>;
 
 export default function AddTagOrList({ isVisible, onClose, onSave, newTag, setNewTag }: Props) {
-    return (
-        <View>
-            <Modal animationType="fade" transparent={true} visible={isVisible}>
-                <View style={styles.modalOverlay}>
-                    <Pressable onPress={Keyboard.dismiss} style={styles.dismissArea}>
-                        <View style={styles.modalContent}>
-                            <View style={styles.titleContainer}>
-                                <Text style={styles.title}>Add a new tag</Text>
-                            </View>
-                            <View style={styles.inputContainer}>
-                                <TextInput 
-                                placeholder="New Tag Name"
-                                placeholderTextColor="#aaaaaa" 
-                                value={newTag} 
-                                onChangeText={setNewTag}
-                                style={styles.input} />
-                            </View>
-                            <View style={styles.bottomButtons}>
-                                <Pressable onPress={onClose} style={styles.cancelBtn}>
-                                    <Text style={styles.cancelText}>Cancel</Text>
-                                </Pressable>
-                                <Pressable onPress={onSave} style={styles.saveBtn}>
-                                    <Text style={styles.saveText}>Save</Text>
-                                </Pressable>
-                            </View>
-                        </View>
-                    </Pressable>
-                </View>
-            </Modal>
+  return (
+    <View>
+      <Modal animationType="fade" transparent={true} visible={isVisible}>
+        <View style={styles.modalOverlay}>
+          <Pressable onPress={Keyboard.dismiss} style={styles.dismissArea}>
+            <View style={styles.modalContent}>
+              <View style={styles.titleContainer}>
+                <Text style={styles.title}>Add a new tag</Text>
+              </View>
+              <View style={styles.inputContainer}>
+                <TextInput
+                  placeholder="New Tag Name"
+                  placeholderTextColor="#aaaaaa"
+                  value={newTag}
+                  onChangeText={setNewTag}
+                  style={styles.input} />
+              </View>
+              <View style={styles.bottomButtons}>
+                <Pressable onPress={onClose} style={styles.cancelBtn}>
+                  <Text style={styles.cancelText}>Cancel</Text>
+                </Pressable>
+                <Pressable onPress={onSave} style={styles.saveBtn}>
+                  <Text style={styles.saveText}>Save</Text>
+                </Pressable>
+              </View>
+            </View>
+          </Pressable>
         </View>
-    );
+      </Modal>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    modalContent: {
-        height: '25%',
-        width: '75%',
-        backgroundColor: '#ffffff',
-        borderRadius: 18,
-        padding: "2%",
+  modalContent: {
+    height: '25%',
+    width: '75%',
+    backgroundColor: '#ffffff',
+    borderRadius: 18,
+    padding: "2%",
 
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 4 },
     shadowOpacity: 0.3,
     shadowRadius: 2,
-    },
-    titleContainer: {
-        height: '16%',
-        backgroundColor: '#ffffff',
-        borderRadius: 10,
-        paddingHorizontal: 20,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    title: {
-        color: '#000000',
-        fontSize: 16,
-    },
-    modalOverlay: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "rgba(0,0,0,0.4)",
-    },
-    input: {
-        borderWidth: 1,
-        borderColor: "#ddd",
-        borderRadius: 8,
-        padding: 10,
-        fontSize: 15,
-        backgroundColor: Colors.light.accentLight,
-        fontFamily: Fonts.regular,
-    },
-    dismissArea: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100%",
-    },
-    bottomButtons: {
-        flexDirection: "row",
-        justifyContent: "space-evenly",
-        alignItems: "center",
-        padding: "5%"
-    },
-    inputContainer: {
-        flex: 1,
-        justifyContent: "center"
-    },
-    cancelBtn: {
+  },
+  titleContainer: {
+    height: '16%',
+    backgroundColor: '#ffffff',
+    borderRadius: 10,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    color: '#000000',
+    fontSize: 16,
+  },
+  modalOverlay: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.4)",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ddd",
+    borderRadius: 8,
+    padding: 10,
+    fontSize: 15,
+    backgroundColor: Colors.light.accentLight,
+    fontFamily: Fonts.regular,
+  },
+  dismissArea: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+  },
+  bottomButtons: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    padding: "5%"
+  },
+  inputContainer: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  cancelBtn: {
     padding: 16,
     backgroundColor: Colors.light.error,
     borderRadius: 999,
