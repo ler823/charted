d4f3e90a856e0be4f0db9785c53101a4d8323a24
samuel <65369878+SamTheMan122@users.noreply.github.com>
2026-04-06 12:56:46 -0700

    feature: added account view without user information (skeleton for now)

---
+++ frontend/app/(tabs)/(account)/edit-account.tsx
@@ -0,0 +1,11 @@
+import { View, StyleSheet } from "react-native";
+
+export default function EditAccount() {
+  return <View style={styles.container} />;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
