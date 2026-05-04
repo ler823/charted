83dc7b2748a0db821d96b20bfb36db968bb395ac
kn2117 <89276194+kn2117@users.noreply.github.com>
2026-05-04 09:27:32 -0700

    Sorts hours, so that all hours pages show with Sunday as the start of the week

--- frontend/app/make-pin.tsx
+++ frontend/app/make-pin.tsx
@@ -585,7 +585,8 @@ export default function MakePin() {
     const detailsRes = await fetch(detailsUrl);
     const detailsJson = await detailsRes.json();
     const hoursUnformatted = detailsJson.result.opening_hours?.periods;
-    const hoursFormatted = (hoursUnformatted ?? []).map((hours) => ({day: hours.close.day, open: hours.open.time, close: hours.close.time}))
+    const hoursFormatted = (hoursUnformatted ?? []).map((hours) => ({day: hours.close.day, open: hours.open.time, close: hours.close.time})).sort((a, b) => {if (a.day < b.day) {return -1} else if (a.day > b.day) {return 1} else {return 0}})
+    console.log(hoursFormatted)
     setHours(hoursFormatted);
     setPlaceId(placeId);
     const formatted =
